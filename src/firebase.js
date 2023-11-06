0ef1a01770d1c316ac99a6944db537baefbe056a
Yogesh Patade <98302621+Yogeshp21@users.noreply.github.com>
2023-11-06 22:33:29 +0530

    fixed deceptive site ahead issue

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC0JXTJSgLYfgnFIK-FNQjch27iZfVWqaw",
-  authDomain: "netflix-clone-f14e6.firebaseapp.com",
-  projectId: "netflix-clone-f14e6",
-  storageBucket: "netflix-clone-f14e6.appspot.com",
-  messagingSenderId: "228005351767",
-  appId: "1:228005351767:web:3e96ccb3b66ed584856e12"
+  apiKey: String(process.env.REACT_APP_API_KEY),
+  authDomain: String(process.env.REACT_APP_AUTH_DOMAIN),
+  projectId: String(process.env.REACT_APP_PROJECT_ID),
+  storageBucket: String(process.env.REACT_APP_STORAGE_BUCKET),
+  messagingSenderId: String(process.env.REACT_APP_API_MESSAGING_SENDER_ID),
+  appId: String(process.env.REACT_APP_APP_ID),
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
